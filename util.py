d89f81a9429de6bc680671039d8f647e244d4679
jonathanturcottte <jonathanturcotte@outlook.com>
2017-11-11 13:46:28 -0500

    Updated util with checkXXXX functions for validating account numbers, amounts, and accounts names

--- util.py
+++ util.py
@@ -37,58 +37,44 @@ class Utility:
         print("Error: " + msg)
 
     #--------------------------------------------------------------------
-    # Get and validate an account number from the user
+    # Validate an account number
     #--------------------------------------------------------------------
     @staticmethod
-    def getAccountNumber(msg):
-        inp = Utility.getInput(msg).strip()
-        if len(inp) != 7 or not inp.isdigit() or inp[:1] == '0':
-            Utility.error("Invalid account number")
-            return None
-        else:
-            return inp
+    def checkAccountNumber(num):
+        if len(num) != 7:
+            Utility.fatal("Account number must be length 7: " + num)
+        elif not num.isdigit():
+            Utility.fatal("Account number not all digits: " + num)
+        elif num[:1] == '0':
+            Utility.fatal("Account number cannot begin with 0: " + num)
 
     #--------------------------------------------------------------------
-    # Get and validate an amount from the user
+    # Validate an amount
     #--------------------------------------------------------------------
     @staticmethod
-    def getAmount(loginType, msg):
-        inp = Utility.getInput(msg).strip()
-        if not inp.isdigit():
-            Utility.error("Invlaid amount")
-            return None
-        amount = int(inp)
-        if amount < 1:
-            Utility.error("Invalid amount") #Urility
-            return None
-        if loginType == "machine" and amount > 100000:
-            Utility.error("Amount cannot be over $1000 when logged in as machine")
-            return None
-        if amount > 99999999:
-            Utility.error("Amount cannot be more than 8 digits")
-            return None
-        return inp
+    def checkAmount(amount):
+        if not amount.isdigit():
+            Utility.fatal("Amount is not digits: " + amount)
+        elif len(amount) < 3:
+            Utility.fatal("Amount not padded to three length: " + amount)
+
+        num = int(amount)
+        if num < 1:
+            Utility.fatal("Amount must be 1 or greater: " + amount)
 
     #--------------------------------------------------------------------
-    # Get and validate an account name from the user
+    # Validate an account name
     #--------------------------------------------------------------------
     @staticmethod
-    def getAccountName(msg):
-        inp = Utility.getInput(msg)
-        if (len(inp) < 3):
-            Utility.error("Account name must be at least 3 characters")
-            return None
-        elif (len(inp) > 30):
-            Utility.error("Account name must be less than 30 characters")
-            return None
-        elif (re.match('^[a-zA-Z0-9 ]+$', inp) is None):
-            Utility.error("Account name can only contain alphanumeric characters")
-            return None
-        elif (inp[0] == ' ' or inp[len(inp) - 1] == ' '):
-            Utility.error("Account name cannot be prefixed or end in a space")
-            return None
-        else:
-            return inp
+    def checkAccountName(name):
+        if (len(name) < 3):
+            Utility.fatal("Account name must be at least 3 characters: " + name)
+        elif (len(name) > 30):
+            Utility.fatal("Account name must be less than 30 characters: " + name)
+        elif (re.match('^[a-zA-Z0-9 ]+$', name) is None):
+            Utility.fatal("Account name can only contain alphanumeric characters: " + name)
+        elif (name[0] == ' ' or name[len(name) - 1] == ' '):
+            Utility.fatal("Account name cannot be prefixed or end in a space: " + name)
 
     @staticmethod
     def log(msg):
